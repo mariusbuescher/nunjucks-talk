1301c5f927c6cf8183848673973c20a7acf24c14
Marius Büscher <marius.buescher@gmx.de>
2015-11-22 13:54:02 +0100

    Einfachen Nunjuckr Task konfigurieren

--- grunt/nunjuckr.js
+++ grunt/nunjuckr.js
@@ -6,5 +6,19 @@
 
 module.exports = function( grunt, options ) {
     return {
+        beispiel: {
+            options: {
+                data: grunt.file.readJSON( options.srcPath + '_data/data.json' ),
+                ext: '.html'
+            },
+            files: [
+                {
+                    expand: true,
+                    cwd: '<%= srcPath %>components/site/templates',
+                    src: '**/*.njs',
+                    dest: '<%= distPath %>'
+                }
+            ]
+        }
     };
 };
---
+++ src/components/site/templates/index.njs
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Nunjucks Talk</title>
+</head>
+<body>
+
+    <h1>{{ title }}</h1>
+    <p>{{ content }}</p>
+
+</body>
+</html>
